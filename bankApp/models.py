bc271151093d1767fd79dbe8e6a4564802eb4e80
sviatosik <ylsvetik@gmail.com>
2022-07-26 13:55:29 +0300

    26.07.2022

--- bankApp/models.py
+++ bankApp/models.py
@@ -8,8 +8,10 @@ class Client(models.Model):
     client_name = models.CharField(max_length=40)
     client_thirname = models.CharField(max_length=40)
     client_age = models.CharField(max_length=40, default=0)
+    client_gender = models.CharField(max_length=40, default="NONE")
     cart_number = models.IntegerField(null=True)
     cart_balance = models.IntegerField(default=0)
 
+
     def __str__(self):
-        return self.client_name, self.client_thirname, self.cart_balance, self.cart_number, self.client_age
+        return f'{self.client_name},{self.client_thirname},{self.cart_balance},{self.cart_number}, {self.client_age}, {self.client_gender}'
